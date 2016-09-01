5c0bff099ccb069e8d23841a2cb6ed5f53760d22
Sarah Plane <sarahmplane@gmail.com>
2016-08-31 20:50:13 -0600

    landing page setup

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe PagesController, :type => :controller do
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
+
+    it "renders the index template" do
+      get :home
+      expect(response).to render_template("home")
+    end
   end
 end

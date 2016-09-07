091bba212162f63f3924a51521fc06f4d22eeb6d
Sarah Plane <sarahmplane@gmail.com>
2016-09-07 15:27:03 -0600

    Product migration

---
+++ app/controllers/product_controller.rb
@@ -0,0 +1,43 @@
+class ProductController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destory]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      flash [:notice] = "Product successfully added."
+      redirect_to products_path
+    else
+      flash.now[:alert] = "Item NOT added, please try again."
+      render :new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :description, :price)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
     get :home
   end
 
+  resources :products
+
   root 'pages#home'
 end

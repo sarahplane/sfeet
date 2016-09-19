105efa21d0104fb2ca2c7cc9b76e086e578ef10f
Sarah Plane <sarahmplane@gmail.com>
2016-09-19 11:14:02 -0600

    pages bootstrap application

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def about
+  end
+
+  def faq
+  end
+
+  def help_center
+  end
+
+  def news
+  end
+
+  def black_list
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   controller :pages do
     get :home
+    get :about
+    get :faq
+    get :help_center
+    get :news
+    get :black_list
   end
 
   resources :products
